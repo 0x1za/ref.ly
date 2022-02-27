e9aeb8ed26caad7706c62660b7585c259857783d
Mwiza Simbeye (CSI) <mwiza.simbeye@fao.org>
2022-02-27 03:35:54 +0200

    Finshing up user functional tests

--- refs/tests/functional/test_users_api.py
+++ refs/tests/functional/test_users_api.py
@@ -5,7 +5,7 @@ def test_index(test_client):
     assert test_client.get("/").status_code == 200
 
 
-def test_create_users_valid(test_client):
+def test_create_users_valid(test_client, init_database):
     response = test_client.post(
         "/v1/create/user", json={"username": "fibby", "email": "Fibby@example.com"}
     )
@@ -26,6 +26,16 @@ def test_create_users_email_not_provided(test_client):
     assert data["errors"][0] == "A required key 'email' was not provided."
 
 
+def test_create_users_username_not_provided(test_client):
+    response = test_client.post(
+        "/v1/create/user", json={"username": "johndoe", "email": "johndoes@example.com"}
+    )
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["status"] == 0
+    assert data["errors"][0] == "UNIQUE constraint failed: user.username"
+
+
 def test_create_user_email_already_exists(test_client):
     response = test_client.post(
         "/v1/create/user", json={"username": "Fibby", "email": "johndoe@example.com"}
@@ -34,13 +44,3 @@ def test_create_user_email_already_exists(test_client):
     assert response.status_code == 200
     assert data["status"] == 0
     assert data["errors"][0] == "UNIQUE constraint failed: user.email"
-
-
-def test_create_user_username_already_exists(test_client):
-    response = test_client.post(
-        "/v1/create/user", json={"username": "johndoe", "email": "example@example.com"}
-    )
-    data = json.loads(response.get_data(as_text=True))
-    assert response.status_code == 200
-    assert data["status"] == 0
-    assert data["errors"][0] == "UNIQUE constraint failed: user.username"
