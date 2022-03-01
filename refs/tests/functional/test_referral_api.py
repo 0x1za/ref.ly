fba589cf36fa452c413489908a20b4a1ca96a25d
Mwiza Simbeye (CSI) <mwiza.simbeye@fao.org>
2022-03-01 12:20:10 +0200

    Adding functional test, send referral to self.

--- refs/tests/functional/test_referral_api.py
+++ refs/tests/functional/test_referral_api.py
@@ -29,11 +29,38 @@ def test_referral_record_already_exist(test_client, init_database):
         },
     )
     data = json.loads(response.get_data(as_text=True))
-    print(data)
     assert response.status_code == 200
     assert data["data"] == {}
     assert (
         data["errors"][0]
         == "You have already invited user with email `invited_user@example.com`"
     )
+    assert data["status"] == 0
+
+
+def test_create_referral_valid(test_client, init_database):
+    response = test_client.post(
+        "/v1/create/referral",
+        json={
+            "invitee_email": "invited_user_2@example.com",
+            "referer_email": "johndoe@example.com",
+        },
+    )
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["message"] == "Referral successfully created"
+    assert data["status"] == 1
+
+
+def test_create_referral_to_self(test_client, init_database):
+    response = test_client.post(
+        "/v1/create/referral",
+        json={
+            "invitee_email": "johndoe@example.com",
+            "referer_email": "johndoe@example.com",
+        },
+    )
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["errors"][0] == "You cannot send a referral to yourself."
     assert data["status"] == 0
--- refs/tests/functional/test_users_api.py
+++ refs/tests/functional/test_users_api.py
@@ -1,7 +1,5 @@
 import json
 
-from refs.models import User
-
 
 def test_index(test_client):
     assert test_client.get("/").status_code == 200
@@ -30,19 +28,16 @@ def test_create_users_email_not_provided(test_client):
 def test_create_user_username_not_provided(test_client):
     response = test_client.post("/v1/create/user", json={"email": "x234@example.com"})
     data = json.loads(response.get_data(as_text=True))
-    print(data)
     assert response.status_code == 200
     assert data["status"] == 0
     assert data["errors"][0] == "A required key 'username' was not provided."
 
 
 def test_create_user_email_already_exists(test_client, init_database):
-    print(User.query.filter_by(id=1).first())
     response = test_client.post(
         "/v1/create/user", json={"username": "x123", "email": "johndoe@example.com"}
     )
     data = json.loads(response.get_data(as_text=True))
-    print(data)
     assert response.status_code == 200
     assert data["status"] == 0
     assert data["errors"][0] == "UNIQUE constraint failed: user.email"
@@ -53,7 +48,6 @@ def test_create_user_username_already_exists(test_client, init_database):
         "/v1/create/user", json={"username": "johndoe", "email": "john@example.com"}
     )
     data = json.loads(response.get_data(as_text=True))
-    print(data)
     assert response.status_code == 200
     assert data["status"] == 0
     assert data["errors"][0] == "UNIQUE constraint failed: user.username"
@@ -69,7 +63,6 @@ def test_create_user_with_valid_referral_code(test_client, init_database):
         },
     )
     data = json.loads(response.get_data(as_text=True))
-    print(data)
     assert response.status_code == 200
     assert data["status"] == 1
     assert data["data"]["current_balance"] == "10"
@@ -86,7 +79,6 @@ def test_create_user_with_invalid_referral_code(test_client, init_database):
         },
     )
     data = json.loads(response.get_data(as_text=True))
-    print(data)
     assert response.status_code == 200
     assert data["status"] == 1
     assert data["data"]["current_balance"] == "0"
