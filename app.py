112689890a95e106c919473d27bfd98c6d92350e
Mwiza Simbeye (CSI) <mwiza.simbeye@fao.org>
2022-03-04 03:05:42 +0200

    Adding test for user GET endpoint.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from refs import create_app
 
+# no cover 3.x
 # Call the application factory function to construct a Flask application
 # instance using the development configuration
 app = create_app("flask.cfg")
--- tests/functional/test_users_api.py
+++ tests/functional/test_users_api.py
@@ -5,6 +5,24 @@ def test_index(test_client):
     assert test_client.get("/").status_code == 200
 
 
+def test_get_all_users(test_client, init_database):
+    response = test_client.get("/v1/users")
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["errors"] == []
+    assert data["status"] == 1
+    assert data["message"] == "Query results: 2 records"
+
+
+def test_get_one_user(test_client, init_database):
+    response = test_client.get("/v1/users?id=1")
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data["errors"] == []
+    assert data["status"] == 1
+    assert data["message"] == "Query results: 1 records"
+
+
 def test_create_users_valid(test_client, init_database):
     response = test_client.post(
         "/v1/create/user", json={"username": "x123", "email": "x123@example.com"}
